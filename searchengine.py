86bcb053afc70c9d6061e4ca6ebda4c8a4c07961
Harkann <paulon@crans.org>
2017-04-26 16:34:49 +0200

    Add parser

--- searchengine.py
+++ searchengine.py
@@ -2,22 +2,38 @@
 
 import requests
 import re
+import argparse
+
 
 def cleanhtml(raw_html):
     cleanr = re.compile('<.*?>')
     cleantext = re.sub(cleanr, '', raw_html)
     return cleantext
+
 def cleanspace(texte):
     cleanr = re.compile('[\r\t\f\v ]{3,}')
     cleantext = re.sub(cleanr, '', texte)
     cleanr = re.compile('\n{3,}')
     cleantext = re.sub(cleanr, '', cleantext)
     return cleantext
+
+def get_info(texte):
+    cleanr = re.compile('<!--[\s\S]*?-->')
+    cleantext = re.sub(cleanr, '$$$$$', texte)
+    list_info = cleantext.split('$$$$$')
+    list_info.pop(0)
+    list_info.pop(-1)
+    return list_info
+
 def makerequest(ORF):
     r = requests.get('http://yeastgenome.org/search?q='+ORF+'&is_quick=true')
     content = cleanhtml(r.text)
     return content
 
-content = makerequest('YPR139C')
-list_content = 
-print(cleanspace(content).split('\n'))
+parser = argparse.ArgumentParser(description='Search for the specified ORF on the yeastgenome.org database')
+parser.add_argument('-y','--yeastgenome',help='Make the request on yeastgenome.org', action='store_true')
+parser.add_argument('orf', help='ORF', type=str)
+args = parser.parse_args()
+if args.orf :
+    content = makerequest(args.orf)
+    print(get_info(cleanspace(content)))
