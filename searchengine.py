6f049303bf1611b3585d6284e1c8e662474795f8
Harkann <paulon@crans.org>
2017-05-29 22:17:38 +0200

    compte les erreurs et commentaires code
    Signed-off-by: Harkann <paulon@crans.org>

--- searchengine.py
+++ searchengine.py
@@ -4,6 +4,9 @@ import requests
 import re
 import argparse
 import copy
+##########################################
+# Titres des parties pour parser la page #
+##########################################
 
 overview_titles = [
 	"Standard Name",
@@ -13,15 +16,17 @@ overview_titles = [
 	"Feature Type",
 	"Description",
 	"Name Description",
-	"",
+	"@", #il faut un symbole en plus qui n'est pas dans le texte pour finir la recherche (c'est sale)
 	]
 
 go_titles = [
 	"Molecular Function",
 	"Biological Process",
 	"Cellular Component",
-	"",
+	"@",
 	]
+
+
 
 def cleanhtml(raw_html):
 	'''transforme le html en texte'''
@@ -48,6 +53,7 @@ def get_info(texte):
 	return list_info
 
 def open_parse_input(i_file):
+	'''ouvre le fichier csv de données et renvoie la liste des ORF'''
 	file = open(i_file,"r")
 	list_input=[]
 	for line in file :
@@ -57,11 +63,14 @@ def open_parse_input(i_file):
 
 
 def write_to_file(overview,protein,go):
+	'''Écrit dans le fichier de sortie'''
 	if args.file :
 		file = open(args.file+".csv","a")
 		for i in overview:
-			file.write(i)
-			file.write(",")
+			if i != "@" :
+				file.write(i)
+				file.write(",")
+
 		for i in go:
 			if type(i)== list :
 				if i != []:
@@ -69,12 +78,14 @@ def write_to_file(overview,protein,go):
 						file.write(j)
 						file.write("$")
 			else :
-				file.write(i)
+				if i != "@" :
+					file.write(i)
 			file.write(",")
 		file.write("\n")
 		file.close()
 
 def init_file():
+	'''Initialise le fichier de sortie avec les titres'''
 	write_to_file(overview_titles,[],go_titles)
 
 
@@ -85,11 +96,13 @@ def makerequest(ORF):
 	return content
 
 def parse_and_request(ORF):
+	'''effectue la requête et la parse'''
+	# dans le cas où on veut faire la requête sur yeastgenome
 	if args.yeastgenome :
 		infos_list = get_info(cleanspace(makerequest(ORF)))
+		# Dans le cas où on a pas d'infos (la recherche n'a rien donné), on renvoie None
 		if infos_list == None:
 			return None
-		i = 0
 		#overview section 
 		overview = infos_list[0]
 		overview = overview.split('\n')
@@ -98,9 +111,9 @@ def parse_and_request(ORF):
 		overview_copy = copy.deepcopy(overview)
 		for title in overview_titles :
 			try :
-				while overview_copy[i] != title :
+				while overview_copy[0] != title :
 					if is_precedent :
-						# on vire les ; et les , parceque c'est relou dans les CSV ...
+						# on vire les ; et les , parce-que c'est relou dans les CSV ...
 						overview_clean.append((overview_copy.pop(0)).replace(",","$").replace(";","$"))
 						is_precedent = False
 					else :
@@ -146,7 +159,7 @@ def parse_and_request(ORF):
 		for a, title in enumerate(go_titles) :
 			
 			try :
-				while go_copy[i] != title :
+				while go_copy[0] != title :
 					if is_precedent :
 						# on vire les ; et les , parce que c'est relou dans les CSV ...
 						go_clean[a].append((go_copy.pop(0)).replace(",","$").replace(";","$").replace("Manually Curated",""))
@@ -157,7 +170,7 @@ def parse_and_request(ORF):
 				is_precedent = True
 			except :
 				print("Va niquer ta mère")
-				go_clean.append("")
+				go_clean[a].append("")
 				go_copy = copy.deepcopy(go)
 				is_precedent = False
 
@@ -209,13 +222,15 @@ if args.input :
 	count_input=1
 	list_input = open_parse_input(args.input)
 	total_input = len(list_input)
+	list_erreurs = []
+	count_errors = 0
 	if args.rank :
 		pass
 	else :
 		init_file()
 	preced_input = None
 	for orf_input in list_input :
-		print(count_input,"/",total_input)
+		print(count_input,"(",count_errors,")","/",total_input)
 		if count_input >= args.rank:
 			if orf_input != preced_input:
 				result=parse_and_request(orf_input)
@@ -224,6 +239,8 @@ if args.input :
 					write_to_file(overview,protein,go)
 				else :
 					print("Erreur sur : ",orf_input)
+					list_erreurs.append(orf_input)
+					count_errors+=1
 		count_input+=1
 		preced_input = orf_input
 
